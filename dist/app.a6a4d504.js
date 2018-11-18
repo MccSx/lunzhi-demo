321ba9374017abd4107c665f8dddf591ecf28bf0
MccSx <shixiangcool@163.com>
2018-11-18 14:32:36 +0800

    使用插槽

--- dist/app.a6a4d504.js
+++ dist/app.a6a4d504.js
@@ -11330,6 +11330,8 @@ exports.default = void 0;
 //
 //
 //
+//
+//
 var _default = {};
 exports.default = _default;
         var $41d9a4 = exports.default || module.exports;
@@ -11344,7 +11346,7 @@ exports.default = _default;
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c("button", { staticClass: "s-button" }, [_vm._v("按钮")])
+  return _c("button", { staticClass: "s-button" }, [_vm._t("default")], 2)
 }
 var staticRenderFns = []
 render._withStripped = true
@@ -11420,7 +11422,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56931" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54768" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
