a3bae85346c9ad45040767ec4d0d9bc2e8a18b38
MccSx <shixiangcool@163.com>
2018-11-24 20:54:17 +0800

    按钮点击事件单元测试

--- src/app.js
+++ src/app.js
@@ -15,17 +15,35 @@ new Vue({
 })
 
 import chai from 'chai'
+import spies from 'chai-spies'
+chai.use(spies)
 const expect = chai.expect
 //单元测试
 {
     const Constructor = Vue.extend(Button)
-    let button = new Constructor({
+    let vm = new Constructor({
         propsData: {
             icon: 'settings'
         }
     })
-    button.$mount('#test')
-    let useElement = button.$el.querySelector('use')
+    vm.$mount('#test')
+    let useElement = vm.$el.querySelector('use')
     let href = useElement.getAttribute('xlink:href')
     expect(href).to.eq('#i-settings')
+    vm.$el.remove()
+    vm.$destroy()
+}
+{
+    const Constructor = Vue.extend(Button)
+    let vm = new Constructor({
+        propsData:{
+            icon:'settings'
+        }
+    })
+    vm.$mount()
+    let spy = chai.spy(function () {})
+    vm.$on('click', spy)
+    let button = vm.$el
+    button.click()
+    expect(spy).to.have.been.called()
 }
