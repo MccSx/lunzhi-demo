916a88a5792ef4ab29ce01be9819a42abdff8ff0
MccSx <shixiangcool@163.com>
2018-11-27 21:49:15 +0800

    添加index.js，导出组件

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ import Button from './src/button'
 import ButtonGroup from './src/button-group'
 import Icon from './src/icon'
 
-expect { Button, ButtonGroup, Icon}
+export { Button, ButtonGroup, Icon}
