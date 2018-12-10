e6bdebfc9891b522618f3ebfb572b3b0a196dfac
MccSx <shixiangcool@163.com>
2018-12-10 21:33:19 +0800

    修改toast样式

--- src/toast.vue
+++ src/toast.vue
@@ -12,9 +12,15 @@
 
 <style lang="scss" scoped>
 .toast{
+    width: 200px;
+    text-align: center;
     background: rgba(0, 0, 0, 0.5);
     color: #fff;
     padding: 10px 20px;
     border-radius: 10px;
+    position: fixed;
+    top: 0;
+    left: 50%;
+    transform: translateX(-50%);
 }
 </style>
