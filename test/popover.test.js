c0503ae2ea31a89f68b556a4304559d047c8501a
MccSx <shixiangcool@163.com>
2019-01-15 21:13:08 +0800

    添加popover测试用例测试可以设置position

--- test/popover.test.js
+++ test/popover.test.js
@@ -11,4 +11,26 @@ describe('Popover', () => {
     it('存在', () => {
         expect(Popover).to.be.ok
     })
+    it('可以设置position', (done) => {
+        Vue.component('x-popover', Popover)
+        const div = document.createElement('div')
+        document.body.appendChild(div)
+        div.innerHTML = `
+        <x-popover position="bottom" ref="xx">
+            <template slot="content">
+            弹出内容
+            </template>
+            <button>点击</button>
+        </x-popover>
+        `
+        const vm = new Vue({
+            el: div
+        })
+        vm.$el.querySelector('button').click()
+        setTimeout(() => {
+            const {contentWrapper} = vm.$refs.xx.$refs
+            expect(contentWrapper.classList.contains('position-bottom')).to.be.true
+            done()
+        }, 200)
+    })
 })
